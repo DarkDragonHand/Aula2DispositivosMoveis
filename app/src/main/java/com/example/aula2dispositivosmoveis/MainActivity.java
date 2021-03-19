d39f24a65555f9f43e58d6e1ae3bb9ba390d37b4
DarkDragonHand <guipotterrp@gmail.com>
2021-03-19 15:06:07 -0300

    Aula 2 DM

--- app/src/main/java/com/example/aula2dispositivosmoveis/MainActivity.java
+++ app/src/main/java/com/example/aula2dispositivosmoveis/MainActivity.java
@@ -17,31 +17,36 @@ public class MainActivity extends AppCompatActivity {
     private TextView resultadoConversao;
     private Button botaoConverter;
 
-    int soma = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         numeroTemperatura = findViewById(R.id.editartexto_numero_1);
+        botaoConverter = findViewById(R.id.botaoConverter);
 
         botaoConverter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 if(numeroTemperatura.getText().toString().length() == 0){
                     numeroTemperatura.setText("0");
                 }
+
                 float num1 = Integer.parseInt(numeroTemperatura.getText().toString());
 
                 Fahreinheit fahreinheit = new Fahreinheit();
                 resultado = fahreinheit.converterCelsiusParaFahreinheit(num1);
 
-                resultadoConversao.setText(String.valueOf(resultado));
+
+                Intent i = new Intent(MainActivity.this, OutraTela.class);
+                i.putExtra("result", resultado);
+
+                startActivity(i);
             }
         });
 
         Log.d("LOG", "onCreate-Tela1");
-        soma++;
     }
 
     @Override
@@ -49,7 +54,6 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
 
         Log.d("LOG", "onStart-Tela1");
-        soma++;
     }
 
     @Override
@@ -57,7 +61,6 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
 
         Log.d("LOG", "onResume-Tela1");
-        
     }
 
 
--- app/src/main/java/com/example/aula2dispositivosmoveis/OutraTela.java
+++ app/src/main/java/com/example/aula2dispositivosmoveis/OutraTela.java
@@ -2,14 +2,18 @@ package com.example.aula2dispositivosmoveis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class OutraTela extends AppCompatActivity {
+    TextView textResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_outra_tela);
         Log.d("LOG", "onCreate-Tela2");
     }
@@ -24,6 +28,16 @@ public class OutraTela extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+
+        float resultado = 0.0f;
+
+        textResultado = (TextView) findViewById(R.id.textResultado);
+        
+        Intent intent = getIntent();
+
+        resultado = intent.getFloatExtra("result", 0);
+
+        textResultado.setText(String.valueOf(resultado));
 
         Log.d("LOG", "onResume-Tela2");
     }
